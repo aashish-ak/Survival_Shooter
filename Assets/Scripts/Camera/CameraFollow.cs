33a2ecb8ba17e31ea30084c4be75f52297fa61d7
aashish-ak <kumaraashish118@gmail.com>
2018-10-04 11:09:22 +0530

    Game Complete

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+    public float Smoothing = 5f;
+    Vector3 offset;
+    private void Start()
+    {
+        offset = transform.position - player.position;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, newPosition, Smoothing * Time.deltaTime );
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 6f;
+    Rigidbody rb;
+    Vector3 move;
+    Animator anim;
+    int floorMask;
+    float camRayLength = 100f;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        floorMask = LayerMask.GetMask("Floor");
+
+    }
+    void FixedUpdate()
+    {
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float yAxis = Input.GetAxisRaw("Vertical");
+        Move(xAxis, yAxis);
+        Turning();
+        Animating(xAxis, yAxis);
+    }
+    void Move(float h, float v)
+    {
+        move.Set(h, 0f, v);
+        move = move.normalized * Speed * Time.deltaTime;
+        rb.MovePosition(transform.position + move);
+    }
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+            Quaternion rotation = Quaternion.LookRotation(playerToMouse);
+            rb.MoveRotation(rotation);
+        }
+    }
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+}
